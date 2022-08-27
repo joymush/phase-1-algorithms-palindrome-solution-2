1dd572b6e6c1e52419a210e55049b50474374c15
joymush <joy.chemutai@student.moringaschool.com>
2022-08-28 00:16:29 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 function isPalindrome(word) {
   // Write your algorithm here
+
+
+  for (let i = 0; i < word.length / 2; i++) {
+
+    const j = word.length - 1 - i;
+    if (word[i] !== word[j]) {
+      return false;
+    }
+  }
+  return true;
 }
-
-/* 
-  Add your pseudocode here
-*/
-
 /*
   Add written explanation of your solution here
 */
@@ -15,6 +20,26 @@ if (require.main === module) {
   // add your own custom tests in here
   console.log("Expecting: true");
   console.log("=>", isPalindrome("racecar"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("mom"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("abba"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("a"));
+
+  console.log("");
+
+  console.log("Expecting: false");
+  console.log("=>", isPalindrome("hi"));
 
   console.log("");
 
